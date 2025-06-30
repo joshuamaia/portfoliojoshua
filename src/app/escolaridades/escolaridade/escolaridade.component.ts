cd96f67d1f332ec473de746c1f64064565ff8518
Joshua Maia Rodrigues <joshua.curriculo@gmail.com>
2025-06-30 16:19:18 -0300

    adicionado MBA de Engenharia de Software

--- src/app/escolaridades/escolaridade/escolaridade.component.ts
+++ src/app/escolaridades/escolaridade/escolaridade.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-escolaridade',
-    templateUrl: './escolaridade.component.html',
-    styleUrls: ['./escolaridade.component.css'],
-    standalone: false
+  selector: 'app-escolaridade',
+  templateUrl: './escolaridade.component.html',
+  styleUrls: ['./escolaridade.component.css'],
+  standalone: false,
 })
-export class EscolaridadeComponent {
-
-}
+export class EscolaridadeComponent {}
